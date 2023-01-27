3212ab804c6e6a6cfa8bacb3545d0c07730f6c12
buddy-codes <vodlsreal@gmail.com>
2023-01-27 16:17:52 -0500

    🎉 turtlebank

---
+++ src/components/TransactionsPage.tsx
@@ -0,0 +1,5 @@
+import { Typography } from "@mui/material";
+
+export default function TransactionsPage() {
+  return <Typography variant="h5">Transactions</Typography>;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import type { AppProps } from "next/app";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark"
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
